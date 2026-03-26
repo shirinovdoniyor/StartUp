51c116e905ba3ef5ecb6a280a23a4ff64debd7b9
Shirinov01 <shirinovdoniyor93@gmail.com>
2026-03-26 21:07:43 +0500

    new

---
+++ services/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+from django.db import models
+from apps.models import Workshop
+
+
+class Problem(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=255)
+    problem = models.ForeignKey(Problem, on_delete=models.CASCADE, related_name='services')
+
+    def __str__(self):
+        return self.name
+
+
+class WorkshopService(models.Model):
+    workshop = models.ForeignKey(Workshop, on_delete=models.CASCADE, related_name='workshop_services')
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, related_name='workshop_services')
+
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.workshop.name} - {self.service.name}"
