750ee3b312e72e70e10d10cbd429a0526a6b6380
Shirinov01 <shirinovdoniyor93@gmail.com>
2026-06-27 17:07:58 +0500

    admin dashboard qoshdim

--- root/settings.py
+++ root/settings.py
@@ -109,7 +109,7 @@ REST_FRAMEWORK = {
 # JWT
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=12),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
