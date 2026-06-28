a611b245e8c22b6581a6390ca85dca23d38f5f66
Sanjarbek <matlabovsanjar38@gmail.com>
2026-06-28 16:07:31 +0500

    changing settings

---
+++ users/migrations/0002_user_add_fields.py
@@ -0,0 +1,34 @@
+# Generated migration to add missing fields
+
+import django.utils.timezone
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='user',
+            name='name',
+            field=models.CharField(blank=True, default='', max_length=255, null=True),
+        ),
+        migrations.AddField(
+            model_name='user',
+            name='created_at',
+            field=models.DateTimeField(auto_now_add=True, default=django.utils.timezone.now),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='user',
+            name='updated_at',
+            field=models.DateTimeField(auto_now=True),
+        ),
+        migrations.RemoveField(
+            model_name='user',
+            name='date_joined',
+        ),
+    ]
