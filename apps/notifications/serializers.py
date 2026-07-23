2b68349fae74a5493bf41af17e66b766f9924583
shirinovdoniyor <shirinovdoniyor93@gmail.com>
2026-07-23 12:45:36 +0500

    .

--- apps/notifications/serializers.py
+++ apps/notifications/serializers.py
@@ -5,6 +5,7 @@ from .models import Notification
 
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
+
         model = Notification
         fields = ["id", "title", "message", "is_read", "created_at", "updated_at"]
         read_only_fields = fields
